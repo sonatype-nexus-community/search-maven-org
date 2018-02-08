6287ed9b92992eeb4cad2b787355b84c1553bcbd
Jeffry Hesse <jeffryxtron@gmail.com>
2018-02-08 12:48:19 -0900

    Public export of search.maven.org code:
      - 3516c433dd6bdeb0bad4beb73fec19f36c8dfebf by Jeffry Hesse <jeffryxtron@gmail.com>
    
    GitOrigin-RevId: 3516c433dd6bdeb0bad4beb73fec19f36c8dfebf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   search: {
-    endpoint : 'http://search.maven.org/central/solrsearch/select'
+    endpoint : 'http://search.maven.org/solrsearch/select'
   },
   stats: {
-    endpoint : 'http://search.maven.org/central/quickstats'
+    endpoint : 'http://search.maven.org/quickstats'
   },
-  smoBaseUrl: 'http://search.maven.org/central/remotecontent?filepath=',
+  smoBaseUrl: 'http://search.maven.org/remotecontent?filepath=',
   repositoryBaseUrl: 'http://repo1.maven.org/maven2'
 };
