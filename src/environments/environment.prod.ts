4ebb5196b2a6e8a2308c4c33e3d147b82453f98c
Moncef <mben-soula@sonatype.com>
2018-04-05 09:42:13 -0700

    Public export of search.maven.org code:
      - 62bf8db6ee2e6920165074fb46a812f63f6d2131 by Moncef <mben-soula@sonatype.com>
    
    GitOrigin-RevId: 62bf8db6ee2e6920165074fb46a812f63f6d2131

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,11 +17,11 @@
 export const environment = {
   production: true,
   search: {
-    endpoint : 'http://search.maven.org/solrsearch/select'
+    endpoint : 'https://search.maven.org/solrsearch/select'
   },
   stats: {
-    endpoint : 'http://search.maven.org/quickstats'
+    endpoint : 'https://search.maven.org/quickstats'
   },
-  smoBaseUrl: 'http://search.maven.org/remotecontent?filepath=',
-  repositoryBaseUrl: 'http://repo1.maven.org/maven2'
+  smoBaseUrl: 'https://search.maven.org/remotecontent?filepath=',
+  repositoryBaseUrl: 'https://repo1.maven.org/maven2'
 };
