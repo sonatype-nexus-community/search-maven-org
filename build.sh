f14aa73374dfeefd857fcc70879252dc3301267f
Jeffry Hesse <5544326+DarthHater@users.noreply.github.com>
2021-10-22 13:25:39 -0800

    Ok, delete everything, setup React

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ng build --prod
-rm dist/assets/data/appConfig.json
-cp dist/assets/data/appConfig-prod.json dist/assets/data/appConfig.json
-rm dist/assets/data/appConfig-prod.json
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
